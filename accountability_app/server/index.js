46e47a2559bfe9dae969ced1768bffd2cefe72eb
AsociTon <asociton@outlook.com>
2020-05-02 19:14:31 +0530

    Connect to MongoDB Atlas.

--- accountability_app/server/index.js
+++ accountability_app/server/index.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
+const mongoose = require('mongoose');
+const {MONGOURI} = require('./keys');
+
+mongoose.connect(MONGOURI);
+
+var db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.log('Connection to MongoDB failed with the error: ', err);
+});
+
+db.once('open', () => {
+  console.log('Connection to MongoDB established');
+});
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
