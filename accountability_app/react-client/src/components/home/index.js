7bff0865e1cb53b026c1f63bd2b36c8d349bd0ad
Ebtesam <ebtesam@iut-dhaka.edu>
2020-05-03 09:01:16 +0600

    Homepage modified

--- accountability_app/react-client/src/components/home/index.js
+++ accountability_app/react-client/src/components/home/index.js
@@ -14,19 +14,19 @@ class Home extends Component {
   } 
     render() {
         return (
-         <div style={{backgroundColor:"#fcf9ec"}}>
+         <div>
             <div>
               <div><img src={LogoImg} alt="logo" width="25%"/></div>
             </div>
             <div>
-              <div style={{margin:"15vh 2.5%",fontFamily:"Raleway", fontSize:"10vh",lineHeight:"100%", color:"#0e9e9b", width:"41.7%", float: "left"}}>
+              <div style={{margin:"15vh 2.5%",fontFamily:"Raleway", fontSize:"9.75vh",lineHeight:"100%", color:"#0e9e9b", width:"41.7%", float: "left"}}>
                 Accomplish your goals today!
-                <div style={{fontFamily:"Palanquin", lineHeight:"100%", fontSize:"5vh", marginTop: "2%"}}>
-                Sign up to find your accountability buddy. It only takes a few minutes!
+                <div style={{fontFamily:"Lato", lineHeight:"100%", fontSize:"3vh", marginTop: "5%"}}>
+                Sign up to find your accountability buddy. Find people who share the same goals and reach new heights together. It only takes a few minutes!
                 </div>
                 <div style={{ marginRight:"5%"}}>
-                <Button type="primary" shape="round" size="large" style={{backgroundColor:"#0d847e", height:"7.5vh", width: "15vh"}}> Sign Up </Button>
-                <Button type="primary" shape="round" size="large" style={{backgroundColor:"#0e9e9b", height:"7.5vh", width: "15vh", marginLeft:"2%"}}> Login </Button>
+                <Button type="primary" shape="round" size="large" style={{backgroundColor:"#0d847e", height:"7.5vh", width: "15vh", border:"none"}}> Sign Up </Button>
+                <Button type="primary" shape="round" size="large" style={{backgroundColor:"#0e9e9b", height:"7.5vh", width: "15vh", marginLeft:"2%", border:"none"}}> Login </Button>
                 </div>
                 </div>
               <div style={{float: "right"}}><img src={HomeImg} alt="home" height="600vh"></img></div>
