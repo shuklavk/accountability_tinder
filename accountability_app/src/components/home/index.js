605b0494b5f5b55560e2e04ca27211327f867392
Vikas Shukla <shuk.vk@gmail.com>
2020-05-03 12:25:27 -0700

    fixes home page css

--- accountability_app/src/components/home/index.js
+++ accountability_app/src/components/home/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 // import '../../css/App.css'
-// import '../../css/style.css'
+import '../../css/home.css'
 //import assets
 import HomeImg from '../../../dist/assests/home.png';
 import LogoImg from '../../../dist/assests/logo.png';
@@ -16,11 +16,11 @@ class Home extends Component {
   } 
     render() {
         return (
-         <div>
+         <div className="homePage">
             <div>
-              <div><img src={LogoImg} alt="logo" width="25%"/></div>
+              <div style={{margin: "20px"}}><img src={LogoImg} alt="logo" width="25%"/></div>
             </div>
-            <div>
+            {/* <div> */}
               <div style={{margin:"15vh 2.5%",fontFamily:"Raleway", fontSize:"9.75vh",lineHeight:"100%", color:"#0e9e9b", width:"41.7%", float: "left"}}>
                 Accomplish your goals today!
                 <div style={{fontFamily:"Lato", lineHeight:"100%", fontSize:"3vh", marginTop: "5%"}}>
@@ -31,9 +31,9 @@ class Home extends Component {
                 <Button type="primary" shape="round" size="large" style={{backgroundColor:"#0e9e9b", height:"7.5vh", width: "15vh", marginLeft:"2%", border:"none"}}> Login </Button>
                 </div>
                 </div>
-              <div style={{float: "right"}}><img src={HomeImg} alt="home" height="600vh"></img></div>
+              <div style={{float: "right"}}><img src={HomeImg} alt="home" height="480vh"></img></div>
             </div>
-         </div>   
+        //  </div>   
         );
     }
 }
